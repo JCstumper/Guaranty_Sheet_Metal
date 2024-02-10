98416153c65c37a2bc421b1d02a0a05b23bea322
brandon-bejarano <brandonbejarano55@gmail.com>
2024-02-10 02:04:05 -0600

    Updated host connection

--- RestApi/database.js
+++ RestApi/database.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: "admin",
     password: "Guaranty123",
     database: "GuarantyDatabase",
-    host: "localhost",
+    host: "ec2-13-58-27-57.us-east-2.compute.amazonaws.com",
     port: 5432
 });
 
