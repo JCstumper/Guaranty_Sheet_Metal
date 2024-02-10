f9548c5c8841f0d26408358dfe13dd1c9b2e2e94
brandon-bejarano <brandonbejarano55@gmail.com>
2024-02-10 01:17:10 -0600

    Reconfigured Express's Connection to PostgresSQL database

--- RestApi/database.js
+++ RestApi/database.js
@@ -1,5 +1,5 @@
 const Pool = require("pg").Pool;
-/*
+
 const pool = new Pool({
     user: "admin",
     password: "Guaranty123",
@@ -7,12 +7,5 @@ const pool = new Pool({
     host: "localhost",
     port: 5432
 });
-*/
-const pool = new Pool({
-    user: "admin",
-    password: "Guaranty123",
-    database: "GuarantyDatabase",
-    host: "ec2-54-89-101-102.compute-1.amazonaws.com",
-    port: 5432
-});
+
 module.exports = pool;
