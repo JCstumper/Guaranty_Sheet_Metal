b9b59c5fd0766eb4615d6ef3973a18db603113cd
unknown <freeze22hockey@gmail.com>
2024-02-12 11:35:58 -0600

    Updates

---
+++ precache-manifest.c57fef26016ffa87bd7c606cdb16c0f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93937de300b9991377b13efdbd291492",
+    "url": "/guaranty_sheet_metal/index.html"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/guaranty_sheet_metal/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/guaranty_sheet_metal/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/guaranty_sheet_metal/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/guaranty_sheet_metal/static/js/main.26948cee.chunk.js"
+  },
+  {
+    "revision": "bee30d9b2ca61904cc17",
+    "url": "/guaranty_sheet_metal/static/js/runtime~main.5d7cd58d.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/guaranty_sheet_metal/static/media/logo.06e73328.svg"
+  }
+]);
