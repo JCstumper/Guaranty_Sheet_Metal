8a67dfa6bf0c21fe40a6e271470f5ab02605ff39
Waleed Kambal <w01.kambal@gmail.com>
2024-02-11 19:43:51 -0600

    Started React Front End

---
+++ FrontEnd/GuarantyMetalReact/src/components/ListGroup.tsx
@@ -0,0 +1,23 @@
+
+
+function ListGroup() {
+    const items = [
+        'Taco',
+        'Penis',
+        'Pizza Hut'
+    ];
+
+   
+    return (
+    <>
+        <h1> List </h1>
+        <ul className="list-group">
+
+       { items.map(item => <li>{item}</li>)}
+
+        </ul>
+    </>
+    )
+}
+
+export default ListGroup;
