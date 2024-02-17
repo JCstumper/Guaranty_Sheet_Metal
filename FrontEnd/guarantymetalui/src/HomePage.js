1ec04eddcbfc9d76be9679587527608dd5645f24
Waleed Kambal <w01.kambal@gmail.com>
2024-02-16 22:38:21 -0600

    working on it

--- FrontEnd/guarantymetalui/src/HomePage.js
+++ FrontEnd/guarantymetalui/src/HomePage.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import logo from './pictures/logo.png'; // Adjust the path according to where you placed the logo
+import { useNavigate } from 'react-router-dom';
+import './LoginPage.css';
+import logo from './pictures/logo.png';
 
-function Header() {
-  return (
-    <header>
-      <nav>
-        <a href="/">
-        <img src={logo} alt="Logo" className="logo" />
-        </a>
-        {/* Rest of the navigation items */}
-      </nav>
-    </header>
-  );
-}
+function HomePage() {
+    
 
-export default Header;
+    return (
+        <div className="login-container">
+            <div className="logo-container">
+                <img src={logo} alt="Company Logo" className="company-logo" />
+            </div>
+            <form className="login-form" onSubmit={handleLogin}>
+                <input type="text" placeholder="Username" className="login-input" />
+                <input type="password" placeholder="Password" className="login-input" />
+                <button type="submit" className="login-button">Login</button>
+                <a href="#" className="forgot-password">Forgot Password?</a>
+            </form>
+        </div>
+    );
+};
+
+
+export default HomePage;
--- FrontEnd/guarantymetalui/src/index.js
+++ FrontEnd/guarantymetalui/src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { useEffect } from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  useEffect(() => {
+    // Dynamically update the document title
+    document.title = "Guaranty Sheet Metal";
+
+    // Dynamically update the favicon
+    const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/png';
+    link.rel = 'icon';
+    link.href = 'gsm_icon.png'; // Ensure this path is correct in your public folder
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }, []);
+
+  return (
+    <div>
+      {/* Your app content here */}
+    </div>
+  );
+};
+
+export default App;
