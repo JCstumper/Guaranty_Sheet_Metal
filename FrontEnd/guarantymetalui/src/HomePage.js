5000410ab445bb84de409dbc7af148fc67df7165
Waleed Kambal <w01.kambal@gmail.com>
2024-02-17 13:33:38 -0600

    edits

--- FrontEnd/guarantymetalui/src/HomePage.js
+++ FrontEnd/guarantymetalui/src/HomePage.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './LoginPage.css';
 import logo from './pictures/logo.png';
 
 function HomePage() {
     
 
     return (
-        <div className="login-container">
+      
             <div className="logo-container">
-                <img src={logo} alt="Company Logo" className="company-logo" />
+                <img src={logo} alt="Company Logo" className="logo" />
             </div>
-            <form className="login-form" onSubmit={handleLogin}>
-                <input type="text" placeholder="Username" className="login-input" />
-                <input type="password" placeholder="Password" className="login-input" />
-                <button type="submit" className="login-button">Login</button>
-                <a href="#" className="forgot-password">Forgot Password?</a>
-            </form>
-        </div>
+           
+     
     );
 };
 
