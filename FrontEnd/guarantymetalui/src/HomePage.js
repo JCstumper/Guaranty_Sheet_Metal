4009b81344641f5f8ae132a2d7c1b029e724ef18
Waleed Kambal <w01.kambal@gmail.com>
2024-02-20 21:02:43 -0600

    fixed the account icon

--- FrontEnd/guarantymetalui/src/HomePage.js
+++ FrontEnd/guarantymetalui/src/HomePage.js
@@ -4,26 +4,21 @@ import logo from "./pictures/logo.png"
 import account from "./pictures/testacc.png"
 import ButtonList from './components/ButtonList';
 
-const buttons = ['DASH', 
-                'INVENTORY', 
-                'ORDERS', 
-                'CUSTOMERS', 
-                <img src= {account} alt="Person" className= "Account" style={{ width: '100%', height: 'auto' }}/>
-                ] // Add more button labels as needed
+const buttons = ['DASH', 'INVENTORY', 'ORDERS', 'CUSTOMERS'];
 
 const Sidebar = () => {
     return (
-      <sidebar className='sidebar-container'>
+      <aside className='sidebar-container'>
         <a href="/">
           <img src={logo} alt="Icon" className="sidebar-image" />
         </a>
         <ButtonList buttons={buttons} />
-      {
-
-      }
-      </sidebar>
+        <button className="Account">
+          <img src={account} alt="Person" style={{ width: '100%', height: 'auto' }} />
+        </button>
+      </aside>
     );
-  };
+};
   
 
 export default Sidebar;
