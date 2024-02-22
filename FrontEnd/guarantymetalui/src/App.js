88fb4ed20ee556ea1c18b5dcb34979cf423cbbd1
Waleed Kambal <w01.kambal@gmail.com>
2024-02-21 20:16:36 -0600

    Merging Brandon and Waleeds stuff

--- FrontEnd/guarantymetalui/src/App.js
+++ FrontEnd/guarantymetalui/src/App.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './LoginPage';
-import HomePage from './HomePage';
+import React, {Fragment, useState, useEffect} from "react";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
+
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
 
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+
+  async function isAuth() {
+    try {
+      
+      const response = await fetch("http://localhost:3000/auth/verify", {
+        method: "GET",
+        headers: {token: localStorage.token}
+      });
+
+      const parseRes = await response.json();
+
+      // console.log(parseRes);
+
+      parseRes === true ? setIsAuthenticated(true): setIsAuthenticated(false);
+
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    isAuth()
+  });
+
   return (
-    <div>
-    <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<Navigate replace to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
+  <Fragment>
+    <Router>
+      <div className="container">
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route exact path="/login" element={!isAuthenticated ? (<Login setAuth={setAuth} />) : (<Navigate to="/dashboard" />)} />
+        <Route exact path="/register" element={!isAuthenticated ? (<Register setAuth={setAuth} />) : (<Login />)} />
+        <Route exact path="/dashboard" element={isAuthenticated ? (<Dashboard setAuth={setAuth} />) : (<Navigate to="/login" />)} />
       </Routes>
-    </BrowserRouter>
-    </div>
-    
+      </div>
+    </Router>
+  </Fragment>
   );
 }
+
 
 export default App;
