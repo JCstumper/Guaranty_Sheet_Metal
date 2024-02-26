27cada5b51e3b5a3ee15d260d335caa1eed99814
unknown <freeze22hockey@gmail.com>
2024-02-25 18:53:35 -0600

    fixed setauth i removed

--- FrontEnd/guarantymetalui/src/Dashboard.js
+++ FrontEnd/guarantymetalui/src/Dashboard.js
@@ -5,7 +5,7 @@ import './Dashboard.css'; // Assuming you have specific styles for Dashboard
 const Dashboard = ({setAuth}) => {
     return (
         <div className="dashboard">
-        <Sidebar />
+        <Sidebar setAuth={setAuth} />
         </div>
     );
 };
