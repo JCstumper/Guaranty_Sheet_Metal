b0792716595bf55e6d666bbd2c3b9ed67ac5aef5
unknown <freeze22hockey@gmail.com>
2024-02-25 20:11:21 -0600

    pweb update with sprint 1 items

--- pweb/src/Deliverables.js
+++ pweb/src/Deliverables.js
@@ -6,7 +6,36 @@ function Deliverables() {
     <div className="deliverables-container">
       <div className="deliverable">
         <h3>Sprint 1 Deliverable</h3>
-        <p>Description or links related to Sprint 1 deliverable...</p>
+        <div>
+          <a href={`${process.env.PUBLIC_URL}/Sprint1PlanningDocument.pdf`} target="_blank" rel="noopener noreferrer">
+            Sprint One Planning Document
+          </a>
+        </div>
+        <div>
+          <a href={`${process.env.PUBLIC_URL}/Sprint1Backlog.pdf`} target="_blank" rel="noopener noreferrer">
+            Sprint One Backlog
+          </a>
+        </div>
+        <div>
+          <a href={`${process.env.PUBLIC_URL}/DailyJournalDocument.pdf`} target="_blank" rel="noopener noreferrer">
+            Daily Journal
+          </a>
+        </div>
+        <div>
+          <a href={`${process.env.PUBLIC_URL}/Sprint1GuarantySheetMetal.pdf`} target="_blank" rel="noopener noreferrer">
+            Sprint One PowerPoint PDF
+          </a>
+        </div>
+        <div>
+          <a href={`${process.env.PUBLIC_URL}/retro.pdf`} target="_blank" rel="noopener noreferrer">
+            Sprint One Retrospective
+          </a>
+        </div>
+        <div>
+          <a href={`${process.env.PUBLIC_URL}/Project1Backlog.pdf`} target="_blank" rel="noopener noreferrer">
+            Project Backlog
+          </a>
+        </div>
       </div>
       <div className="deliverable">
         <h3>Sprint 2 Deliverable</h3>
