d5b7949efaca1c6b10415e5e9ac654d8efa93ba7
Waleed Kambal <w01.kambal@gmail.com>
2024-03-18 19:43:08 -0500

    my dash stuff

--- FrontEnd/guarantymetalui/src/Dashboard.js
+++ FrontEnd/guarantymetalui/src/Dashboard.js
@@ -1,17 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
 import Sidebar from './components/sidebar';
 import OverviewCard from './components/OverviewCard'; // Import OverviewCard component
 import './Dashboard.css';
-=======
 import Topbar from './components/topbar'; // Adjust the path as necessary
 import './Dashboard.css'; // Assuming you have specific styles for Dashboard
->>>>>>> main
 
 const Dashboard = ({setAuth}) => {
     return (
         <div className="dashboard">
-<<<<<<< HEAD
             <Sidebar setAuth={setAuth}/>
             <div className="card-container">
                 {/* Use OverviewCard component for each card */}
@@ -20,9 +16,7 @@ const Dashboard = ({setAuth}) => {
                 <OverviewCard title="Top Product" />
                 <OverviewCard title="Pending Jobs" />
             </div>
-=======
         <Topbar setAuth={setAuth}/>
->>>>>>> main
         </div>
     );
 };
