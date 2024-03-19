2403cda5e029189b76b85e21c8de958fb46dcfdd
Waleed Kambal <w01.kambal@gmail.com>
2024-03-18 19:50:06 -0500

    mroe shit

--- FrontEnd/guarantymetalui/src/components/topbar.js
+++ FrontEnd/guarantymetalui/src/components/topbar.js
@@ -1,44 +1,8 @@
 import React, { useState, useEffect } from 'react';
-<<<<<<< HEAD:FrontEnd/guarantymetalui/src/components/sidebar.js
+FrontEnd/guarantymetalui/src/components/sidebar.js
 import './sidebar.css';
-<<<<<<< HEAD
-import collapsedLogo from "../pictures/collapse-logo.png";
-import ButtonList from './ButtonList';
-import LogoutConfirmation from './LogoutConfirmation';
 
-const buttons = ['HOME', 'INVENTORY', 'ORDERS', 'CUSTOMERS'];
-
-const Sidebar = ({ setAuth }) => {
-    const [activeTab, setActiveTab] = useState(buttons[0]);
-    const [userName, setName] = useState("");
-    const [logoutConfirmationOpen, setLogoutConfirmationOpen] = useState(false);
-    const [initialBgColor, setInitialBgColor] = useState('#d1cece'); // Now correctly placed within the component
-    const [showSettingsDropdown, setShowSettingsDropdown] = useState(false);
-    const toggleSettingsDropdown = () => {
-        setShowSettingsDropdown(prevState => !prevState);
-    };
-    
-    useEffect(() => {
-        // Attempt to fetch an existing color for the user from local storage
-        let color = localStorage.getItem('userColor');
-        if (!color) {
-            // If not found, generate a new one and store it
-            color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-            localStorage.setItem('userColor', color);
-        }
-        setInitialBgColor(color);
-    }, []); // Empty dependency array ensures this runs once on component mount
-    
-    const logout = (e) => {
-        e.preventDefault();
-        setLogoutConfirmationOpen(true);
-    };
-
-    const confirmLogout = () => {
-=======
-=======
 import './topbar.css';
->>>>>>> main:FrontEnd/guarantymetalui/src/components/topbar.js
 import logo from "../pictures/logo.png";
 import { NavLink } from 'react-router-dom';
 import { MdDashboard, MdInventory, MdShoppingCart, MdPeople, MdSettings, MdExitToApp, } from 'react-icons/md';
@@ -85,38 +49,11 @@ const Topbar = ({ setAuth }) => {
     }, []);
 
     const logout = () => {
->>>>>>> origin/main
+
         localStorage.removeItem("token");
         setAuth(false);
     };
 
-<<<<<<< HEAD
-    return (
-        <aside className="sidebar-container">
-            <a href="/" onClick={(e) => e.preventDefault()}>
-                <img src={collapsedLogo} alt="Logo" className="sidebar-image" />
-            </a>
-            <ButtonList buttons={buttons} activeTab={activeTab} setActiveTab={setActiveTab} />
-            <div className="sidebar-footer">
-                <div 
-                    className="user-name-initial" 
-                    style={{ backgroundColor: initialBgColor }}
-                    onClick={toggleSettingsDropdown}
-                >
-                    {userName.charAt(0).toUpperCase()}
-                </div>
-    
-                {/* Conditional rendering for showing the settings/options modal or dropdown */}
-                {showSettingsDropdown && (
-                    <div className="settings-dropdown">
-                        <a href="/settings" className="dropdown-item">Settings</a>
-                        <a href="#" className="dropdown-item" onClick={logout}>Logout</a>
-                    </div>
-                )}
-            </div>
-    
-            <LogoutConfirmation isOpen={logoutConfirmationOpen} onConfirm={confirmLogout} onCancel={() => setLogoutConfirmationOpen(false)} />
-=======
     const generateRandomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
 
     const handleLinkClick = (button) => {
@@ -177,7 +114,6 @@ const Topbar = ({ setAuth }) => {
             </div>
 
             <LogoutConfirmation isOpen={logoutConfirmationOpen} onConfirm={logout} onCancel={() => setLogoutConfirmationOpen(false)} />
->>>>>>> origin/main
         </aside>
     );
     
