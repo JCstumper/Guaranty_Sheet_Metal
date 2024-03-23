173a535263c7381cc7f2ad123eb9389391ef961a
Waleed Kambal <w01.kambal@gmail.com>
2024-03-22 19:20:15 -0500

    dash card

--- FrontEnd/guarantymetalui/src/Dashboard.js
+++ FrontEnd/guarantymetalui/src/Dashboard.js
@@ -6,7 +6,15 @@ const Dashboard = ({setAuth}) => {
     return (
         <div className="dashboard">
         <Topbar setAuth={setAuth}/>
+        <div className="card">
+            <div className="card-content">
+                <h3 className="card-title">Card Title</h3>
+                <p className="card-info">This is a summary of the card.</p>
+            </div>
+            
         </div>
+    </div>
+    
     );
 };
 
