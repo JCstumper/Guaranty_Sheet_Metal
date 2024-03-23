c40b61718fe69f0d32097970cf4c9bbb7a7cb985
Waleed Kambal <w01.kambal@gmail.com>
2024-03-22 19:23:51 -0500

    dash problems

--- FrontEnd/guarantymetalui/src/Dashboard.js
+++ FrontEnd/guarantymetalui/src/Dashboard.js
@@ -4,15 +4,11 @@ import './Dashboard.css'; // Assuming you have specific styles for Dashboard
 
 const Dashboard = ({setAuth}) => {
     return (
-        <div className="dashboard">
+    <div className="dashboard">
         <Topbar setAuth={setAuth}/>
-        <div className="card">
-            <div className="card-content">
-                <h3 className="card-title">Card Title</h3>
-                <p className="card-info">This is a summary of the card.</p>
-            </div>
+        
             
-        </div>
+        
     </div>
     
     );
