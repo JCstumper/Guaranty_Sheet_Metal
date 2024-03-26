f2b8770cac2bac518ec5996bbff6d0f2d08bd854
brandon-bejarano <brandonbejarano55@gmail.com>
2024-03-26 00:42:42 -0500

    Updated Pweb with Sprint 2 Deliverables

--- pweb/src/Deliverables.js
+++ pweb/src/Deliverables.js
@@ -36,19 +36,22 @@ function Deliverables() {
         <a href={`${process.env.PUBLIC_URL}/Sprint2Backlog.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
           Sprint Two Backlog
         </a>
-        <a href={`${process.env.PUBLIC_URL}/DailyJournalDocument.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
-          Updated Daily Journal
+        <a href={`${process.env.PUBLIC_URL}/Sprint2DailyJournal.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
+          Sprint Two Daily Journal
         </a>
-        <a href={`${process.env.PUBLIC_URL}/Metals-Sprint1Slides.pptx`} target="_blank" rel="noopener noreferrer" className="github-button">
+        <a href={`${process.env.PUBLIC_URL}/Sprint2GuarantySheetMetal.pptx`} target="_blank" rel="noopener noreferrer" className="github-button">
           Sprint Two PowerPoint
         </a>
         <a href={`${process.env.PUBLIC_URL}/Sprint2Retrospective.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
           Sprint Two Retrospective
         </a>
-        <a href={`${process.env.PUBLIC_URL}/Project1Backlog.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
+        <a href={`${process.env.PUBLIC_URL}/Sprint2ProjectBacklog.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
           Project Backlog
         </a>
-        <a href="https://github.com/JCstumper/Guaranty_Sheet_Metal/tree/sprint1snapshot" className="github-button">
+        <a href={`${process.env.PUBLIC_URL}/Sprint2TestingDoc.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
+          Test Cases
+        </a>
+        <a href="https://github.com/JCstumper/Guaranty_Sheet_Metal/tree/sprint2snapshot" className="github-button">
           Guaranty Sheet Metal Development GitHub for Sprint 2
         </a>
       </div>
