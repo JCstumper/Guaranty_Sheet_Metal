840dffa1e6bd80b6c80d8cfdd1be00e6afcc1f93
unknown <freeze22hockey@gmail.com>
2024-04-05 20:44:00 -0500

    worked on edit product modal

--- FrontEnd/guarantymetalui/src/components/EditProductModal.js
+++ FrontEnd/guarantymetalui/src/components/EditProductModal.js
@@ -1,17 +1,40 @@
 // EditProductModal.js
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { toast } from 'react-toastify';
 import './AddProduct.css';
 
-const EditProductModal = ({
-    showModal,
-    setShowModal,
-    editProductItem,
-    setEditProductItem,
-    fetchProductsWithInventory,
-    API_BASE_URL
-}) => {
+const EditProductModal = ({ showModal, setShowModal, editProductItem, setEditProductItem, fetchProductsWithInventory, API_BASE_URL }) => {
+    const initialItemType = editProductItem.unit === 'pcs' ? 'box' : 'length';
+    const [itemType, setItemType] = useState(initialItemType);
+
+    useEffect(() => {
+      // Whenever the modal is opened or the item's unit is changed, set the itemType
+        if (showModal) {
+            const currentType = editProductItem.unit === 'pcs' ? 'box' : 'length';
+            setItemType(currentType);
+        }
+    }, [showModal, editProductItem.unit]);
+
     if (!showModal) return null;
+
+    const validateDollarAmount = value => {
+        const regex = /^\d*\.?\d{0,2}$/;
+        return regex.test(value);
+    };
+
+    const handlePriceChange = (e) => {
+        const value = e.target.value.replace(/^\$/, ''); // Remove dollar sign at the start
+        if (validateDollarAmount(value)) {
+            setEditProductItem({ ...editProductItem, price: value });
+        }
+    };
+    
+    const handleMarkupPriceChange = (e) => {
+        const value = e.target.value.replace(/^\$/, ''); // Remove dollar sign at the start
+        if (validateDollarAmount(value)) {
+            setEditProductItem({ ...editProductItem, markUpPrice: value });
+        }
+    };
 
     const handleUpdateProduct = async (e) => {
         e.preventDefault();
@@ -70,7 +93,6 @@ const EditProductModal = ({
         }
     };
 
-
     // You can keep the existing form and input handlers intact
 
     return (
@@ -82,71 +104,142 @@ const EditProductModal = ({
                 </div>
                 <div className="modal-body">
                     <form onSubmit={handleUpdateProduct}>
-                        <input
-                            type="text"
-                            placeholder="Part Number"
-                            value={editProductItem.partNumber}
-                            onChange={e => setEditProductItem({ ...editProductItem, partNumber: e.target.value })}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Supplier Part Number"
-                            value={editProductItem.supplierPartNumber}
-                            onChange={e => setEditProductItem({ ...editProductItem, supplierPartNumber: e.target.value })}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Radius Size"
-                            value={editProductItem.radiusSize}
-                            onChange={e => setEditProductItem({ ...editProductItem, radiusSize: e.target.value })}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Material Type"
-                            value={editProductItem.materialType}
-                            onChange={e => setEditProductItem({ ...editProductItem, materialType: e.target.value })}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Color"
-                            value={editProductItem.color}
-                            onChange={e => setEditProductItem({ ...editProductItem, color: e.target.value })}
-                        />
-                        <textarea
-                            placeholder="Description"
-                            value={editProductItem.description}
-                            onChange={e => setEditProductItem({ ...editProductItem, description: e.target.value })}
-                        ></textarea>
-                        <input
-                            type="text"
-                            placeholder="Type"
-                            value={editProductItem.type}
-                            onChange={e => setEditProductItem({ ...editProductItem, type: e.target.value })}
-                        />
-                        <input
-                            type="number"
-                            placeholder="Quantity of Item"
-                            value={editProductItem.quantityOfItem}
-                            onChange={e => setEditProductItem({ ...editProductItem, quantityOfItem: e.target.value })}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Unit"
-                            value={editProductItem.unit}
-                            onChange={e => setEditProductItem({ ...editProductItem, unit: e.target.value })}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Price"
-                            value={editProductItem.price}
-                            onChange={e => setEditProductItem({ ...editProductItem, price: e.target.value })}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Mark Up Price"
-                            value={editProductItem.markUpPrice}
-                            onChange={e => setEditProductItem({ ...editProductItem, markUpPrice: e.target.value })}
-                        />
+                        <div className="form-group">
+                            <label htmlFor="part-number">Part Number:</label>
+                            <input
+                                id="part-number"
+                                type="text"
+                                placeholder="Part Number"
+                                value={editProductItem.partNumber}
+                                onChange={e => setEditProductItem({ ...editProductItem, partNumber: e.target.value })}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="supplier-part-number">Supplier Part Number:</label>
+                            <input
+                                id="supplier-part-number"
+                                type="text"
+                                placeholder="Supplier Part Number"
+                                value={editProductItem.supplierPartNumber}
+                                onChange={e => setEditProductItem({ ...editProductItem, supplierPartNumber: e.target.value })}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="radius-size">Radius Size:</label>
+                            <input
+                                id="radius-size"
+                                type="text"
+                                placeholder="Radius Size"
+                                value={editProductItem.radiusSize}
+                                onChange={e => setEditProductItem({ ...editProductItem, radiusSize: e.target.value })}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="material-type">Material Type:</label>
+                            <input
+                                id="material-type"
+                                type="text"
+                                placeholder="Material Type"
+                                value={editProductItem.materialType}
+                                onChange={e => setEditProductItem({ ...editProductItem, materialType: e.target.value })}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="color">Color:</label>
+                            <input
+                                id="color"
+                                type="text"
+                                placeholder="Color"
+                                value={editProductItem.color}
+                                onChange={e => setEditProductItem({ ...editProductItem, color: e.target.value })}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="description">Description:</label>
+                            <textarea
+                                id="description"
+                                placeholder="Description"
+                                value={editProductItem.description}
+                                onChange={e => setEditProductItem({ ...editProductItem, description: e.target.value })}
+                            />
+                        </div>
+                        <div className="category-fields">
+                            <div className="form-group category">
+                                <label htmlFor="type">Category:</label>
+                                <input
+                                    id="type"
+                                    type="text"
+                                    placeholder="Category"
+                                    value={editProductItem.type}
+                                    onChange={e => setEditProductItem({ ...editProductItem, type: e.target.value })}
+                                />
+                            </div>
+                            <div className="form-group catcode">
+                                <label htmlFor="catcode">CatCode:</label>
+                                <input
+                                    id="catcode"
+                                    type="text"
+                                    placeholder="CatCode"
+                                    value={editProductItem.catCode} // Make sure this state exists
+                                />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="item-type">Item Type:</label>
+                            <div className="custom-select-wrapper">
+                                <select
+                                    id="item-type"
+                                    className="custom-select"
+                                    value={itemType}
+                                    onChange={e => setItemType(e.target.value)}
+                                >
+                                    <option value="box">Box Item</option>
+                                    <option value="length">Length Item</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="quantity-of-item">{itemType === 'box' ? 'Quantity' : 'Length'}:</label>
+                            <div className="input-with-unit">
+                                <input
+                                    id="quantity-of-item"
+                                    type="text"
+                                    placeholder={itemType === 'box' ? 'Enter quantity' : 'Enter length'}
+                                    value={editProductItem.quantityOfItem}
+                                    onChange={e => setEditProductItem({ ...editProductItem, quantityOfItem: e.target.value })}
+                                    className="quantity-input"
+                                />
+                                <span className="unit-label">{itemType === 'box' ? 'pcs' : 'ft'}</span>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="price">Price:</label>
+                            <div className="input-wrapper">
+                                <input
+                                    id="price"
+                                    type="text"
+                                    placeholder="Price"
+                                    value={editProductItem.price.replace(/^\$/, '')} // Ensure the dollar sign is removed
+                                    onChange={handlePriceChange}
+                                    className="dollar-input"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="mark-up-price">Mark up Price:</label>
+                            <div className="input-wrapper">
+                                <input
+                                    id="mark-up-price"
+                                    type="text"
+                                    placeholder="Mark Up Price"
+                                    value={editProductItem.markUpPrice.replace(/^\$/, '')} // Ensure the dollar sign is removed
+                                    onChange={handleMarkupPriceChange}
+                                    className="dollar-input"
+                                />
+                            </div>
+                        </div>
                         <div className="modal-actions">
                             <button type="submit">Save Changes</button>
                             <button type="button" onClick={() => setShowModal(false)}>Cancel</button>
