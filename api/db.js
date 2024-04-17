6befcdcc5737abe711c228f88e42da2f2ae74445
brandon-bejarano <brandonbejarano55@gmail.com>
2024-04-16 20:20:58 -0500

    Moved Environment Variables
    Moved the environment variables from the docker-compose.yml and docker-compose.production.yml files to a .env file in the root folder.

--- api/db.js
+++ api/db.js
@@ -3,10 +3,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
+  user: process.env.POSTGRES_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
   port: process.env.DB_PORT,
 });
 
