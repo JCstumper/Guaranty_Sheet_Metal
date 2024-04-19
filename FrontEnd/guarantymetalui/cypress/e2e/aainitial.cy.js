e5ef1ee2267a9daa247f2fe55024aa1e78a54dbb
unknown <freeze22hockey@gmail.com>
2024-04-19 13:40:38 -0500

    had to fix testing

--- FrontEnd/guarantymetalui/cypress/e2e/aainitial.cy.js
+++ FrontEnd/guarantymetalui/cypress/e2e/aainitial.cy.js
@@ -2,23 +2,6 @@ describe('Do the initial setup of a user', () => {
     beforeEach(() => {
         cy.viewport(1280, 720);
 
-        cy.intercept('GET', '/api/auth/verify', {
-            statusCode: 401,
-            body: {
-                authenticated: false,
-            },
-        }).as('verifyAuth');
-
-        cy.visit('https://localhost');
-
-        cy.wait('@verifyAuth');
-
-        cy.get('input[name="username"]').type('admin');
-        cy.get('input[name="password"]').type('Admin123!');
-        cy.get('.login-button').click();
-
-        cy.contains('Login Successful!').should('be.visible');
-
         cy.wait(1000);
     });
 
