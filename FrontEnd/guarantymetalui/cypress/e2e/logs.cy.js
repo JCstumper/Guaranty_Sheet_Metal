0d7fcdac37e0af4fe48ea7afe6369bc2dc45b78e
brandon-bejarano <brandonbejarano55@gmail.com>
2024-04-22 16:24:31 -0500

    Created More Tests for the Logs Page

--- FrontEnd/guarantymetalui/cypress/e2e/logs.cy.js
+++ FrontEnd/guarantymetalui/cypress/e2e/logs.cy.js
@@ -24,7 +24,7 @@ describe('Logging the action of adding a new user to the database', () => {
         });
     });
 
-    it('Successfully add a new user and verify in the logs page', () => {
+    it('Successfully Add a New User and Verify in the Logs Page', () => {
         cy.get('.username').click();
         cy.get('.add-a-user').contains('Add a User').click();
 
@@ -65,7 +65,7 @@ describe('Logging the action of adding a new user to the database', () => {
         cy.contains('User successfully removed').should('be.visible');
     });
 
-    it('Successfully add product and verify in the logs page', () => {   
+    it('Successfully Add Product and Verify in the Logs Page', () => {   
         cy.contains('INVENTORY').click();
         cy.get('.add-button').click();
 
@@ -99,7 +99,215 @@ describe('Logging the action of adding a new user to the database', () => {
         cy.contains('supplierPartNumber: GTY-66498 radiusSize: 5 materialType: Copper color: Copper description: This is a test product description type: Widget quantityOfItem: 15 unit: ft price: 10.20 markUpPrice: 13.26 catCode: WGT').should('be.visible');
     });
 
-    it('Successfully edit product and verify in the logs page', () => { 
+    it('Successfully Add a Job and Verify in the Logs Page', () => {   
+        cy.contains('JOBS').click();
+        cy.get('.add-button').contains('+').click();
+
+        cy.get('input[id="customer_name"]').type('Test 1');
+        cy.get('input[id="address"]').type('123 Playground Stret');
+        cy.get('input[id="phone"]').type('555-555-5555');
+        cy.get('input[id="email"]').type('test1@gmail.com');
+
+        cy.contains('Add Job').click();
+
+        cy.contains('LOGS').click();
+
+        cy.contains('Add Job').scrollIntoView().should('be.visible');
+        cy.contains('Job Management').should('be.visible');
+        cy.contains('Added job_id: 3 customer_name: Test 1 address: 123 Playground Stret phone: 555-555-5555 email: test1@gmail.com').should('be.visible');
+    });
+
+    it('Successfully Add Product C5WGT15 to Necessary Parts and Verify in Logs Page', () => {
+        cy.contains('JOBS').click();
+        cy.contains('Test 1').click();
+    
+        // Target the specific section before clicking the 'Add Part' button
+        cy.contains('div', 'Necessary Parts').within(() => {
+            cy.get('.add-part-btn').click();
+        });
+        cy.get('.form-control').type('C5WGT15');
+        
+        cy.get('.btn-primary').contains('Add').click();
+
+        cy.contains('LOGS').click();
+
+        cy.contains('Add Necessary Part').scrollIntoView().should('be.visible');
+        cy.contains('Necessary Parts').should('be.visible');
+        cy.contains('Add necessary part').should('be.visible');
+    });
+
+    it('Successfully Edit Product C5WGT15 in Necessary Parts and Verify in the Logs Page', () => {
+        cy.contains('JOBS').click();
+        cy.contains('Test 1').click();
+    
+        // Navigate to the Necessary Parts section and initiate editing
+        cy.contains('div', 'Necessary Parts').within(() => {
+            cy.get('.details-btn').contains('Edit').click();
+    
+            cy.get('input[type="number"]').clear().type('500');
+        });
+
+        cy.get('.details-btn').contains('Save').click();
+
+        cy.get('.btn-primary').contains('Update').click();
+
+        cy.contains('Part updated successfully').should('be.visible');
+
+        cy.get('.Toastify__toast-container').within(() => {
+            cy.get('.Toastify__close-button').click();
+        });
+
+        cy.contains('LOGS').click();
+
+        cy.contains('Update Necessary Part').scrollIntoView().should('be.visible');
+        cy.contains('Necessary Parts').should('be.visible');
+        cy.contains('Update id: 3 job_id: 3 part_number: C5WGT15 quantity_required: 500').should('be.visible');
+    });
+
+    it('Successfully Move Product C5WGT15 from Necessary Parts to Used Parts and Verify in the Logs Page', () => {
+        cy.contains('INVENTORY').click();
+        cy.contains('C5WGT15').click();
+
+        cy.get('.edit-button').contains('Edit Quantity').click();
+
+        cy.get('.edit-quantity-modal-input').clear().type('50');
+        
+        cy.get('.edit-quantity-modal-update-btn').contains('Update').click();
+        cy.contains('Quantity updated successfully').should('be.visible');
+
+        cy.get('.Toastify__toast-container').within(() => {
+            cy.get('.Toastify__close-button').click();
+        });
+        
+        cy.contains('JOBS').click();
+        cy.contains('Test 1').click();
+    
+        // Navigate to the Necessary Parts section and initiate editing
+        cy.contains('div', 'Necessary Parts').within(() => {
+            cy.get('.details-btn').contains('Move to Used').click();
+        });
+
+        cy.get('.btn-primary').contains('Confirm').click();
+
+        cy.contains('Part moved to used successfully.').should('be.visible');
+        cy.contains('50 of C5WGT15 moved.').should('be.visible');
+
+        cy.get('.Toastify__toast-container').within(() => {
+            cy.get('.Toastify__close-button').click();
+        });
+        
+        cy.contains('LOGS').click();
+
+        cy.contains('Move to Used').scrollIntoView().should('be.visible');
+        cy.contains('Part Movement').should('be.visible');
+        cy.contains('Part moved to used successfully. 50 of C5WGT15 moved.').should('be.visible');
+    });
+
+    it('Successfully Returned Product C5WGT15 From Used Parts to Necessary Parts and Verify in the Logs Page', () => {
+        cy.contains('JOBS').click();
+        cy.contains('Test 1').click();
+    
+        // Navigate to the Necessary Parts section and initiate editing
+        cy.contains('div', 'Used Parts').within(() => {
+            cy.get('.details-btn').contains('Return to Necessary').click();
+        });
+        
+        cy.get('.btn-primary').contains('Return to Necessary').click();
+        cy.contains('Part returned to necessary successfully').should('be.visible');
+
+        cy.get('.Toastify__toast-container').within(() => {
+            cy.get('.Toastify__close-button').click();
+        });
+
+        cy.contains('LOGS').click();
+
+        cy.contains('Return to Necessary').scrollIntoView().should('be.visible');
+        cy.contains('Part Management').should('be.visible');
+        cy.contains('Part returned to necessary successfully job_id: 3 part_number: C5WGT15 quantity_returned: 50 ').should('be.visible');
+    });
+
+    it('Successfully Remove Product C5WGT15 from Used Parts and Verify in the Logs Page', () => {
+        cy.contains('JOBS').click();
+        cy.contains('Test 1').click();
+    
+        cy.contains('div', 'Necessary Parts').within(() => {
+            cy.get('.details-btn').contains('Move to Used').click();
+        });
+
+        cy.get('.btn-primary').contains('Confirm').click();
+
+        cy.contains('Part moved to used successfully.').should('be.visible');
+        cy.contains('50 of C5WGT15 moved.').should('be.visible');
+
+        cy.get('.Toastify__toast-container').within(() => {
+            cy.get('.Toastify__close-button').click();
+        });
+
+        // Navigate to the Necessary Parts section and initiate editing
+        cy.contains('div', 'Used Parts').within(() => {
+            cy.get('.details-btn').contains('Remove').click();
+        });
+
+        cy.get('.btn-primary').contains('Remove Part').click();
+        cy.contains('Part C5WGT15 removed from used.').should('be.visible');
+
+        cy.get('.Toastify__toast-container').within(() => {
+            cy.get('.Toastify__close-button').first().click();
+        });
+
+        cy.contains('LOGS').click();
+
+        cy.contains('Delete From Used').scrollIntoView().should('be.visible');
+        cy.contains('Part Management').should('be.visible');
+        cy.contains('Part C5WGT15 removed from used and quantity added back to inventory. part_number: C5WGT15 quantity_in_stock: 50 ').should('be.visible');
+    });
+
+    it('Successfully Edit a Job and Verify in the Logs Page', () => {
+        cy.contains('JOBS').click();
+
+        cy.contains('tr', 'Test 1').within(() => {
+            cy.get('.edit-btn').click(); 
+        });    
+
+        cy.get('input[id="customer_name"]').clear().type('Test 2');
+        cy.get('input[id="address"]').clear().type('123 Edit Street');
+        cy.get('input[id="phone"]').clear().type('666-666-6666');
+        cy.get('input[id="email"]').clear().type('Testing123@Edit.com');
+        
+        cy.get('.btn-primary').contains('Save Changes').click();
+
+        cy.wait(1000);
+
+        cy.contains('LOGS').click();
+
+        cy.contains('Update Job').scrollIntoView().should('be.visible');
+        cy.contains('Job Management').should('be.visible');
+        cy.contains('Update job_id: 3 customer_name: Test 2 address: 123 Edit Street phone: 666-666-6666 email: Testing123@Edit.com').should('be.visible');
+    });
+
+    it('Successfully Delete Job and Verify in the Logs Page', () => {
+        cy.contains('JOBS').click();
+
+        cy.contains('tr', 'Test 2').within(() => {
+            cy.get('.remove-btn').click(); // Click the edit button within this row
+        });
+
+        cy.get('.btn-primary').click();
+
+        cy.contains('Job 3 removed successfully').should('be.visible');
+
+        cy.get('.Toastify__toast-container').within(() => {
+            cy.get('.Toastify__close-button').click();
+        });
+
+        cy.contains('LOGS').click();
+
+        cy.contains('Delete Job').scrollIntoView().should('be.visible');
+        cy.contains('Job Management').should('be.visible');
+        cy.contains('Job at address 123 Edit Street removed successfully job_id: 3 customer_name: Test 2 address: 123 Edit Street phone: 666-666-6666 email: Testing123@Edit.com').should('be.visible');
+    });
+
+    it('Successfully Edit Product and Verify in the Logs Page', () => { 
         cy.contains('INVENTORY').click();
 
         cy.get('.add-button').click();
@@ -152,10 +360,10 @@ describe('Logging the action of adding a new user to the database', () => {
 
         cy.contains('Update Product').scrollIntoView().should('be.visible');
         cy.contains('Inventory').should('be.visible');
-        cy.contains('Product Information Updated partNumber: A25NEW889 supplierPartNumber: BrandNewPart123 radiusSize: 25 materialType: Aluminum color: Gray description: Updated item type: NewCategory oldType: Widget quantityOfItem: 80 unit: pcs price: 60 markUpPrice: 300 catCode: NEW889').scrollIntoView().should('be.visible');
+        cy.contains('Product Information Updated partNumber: A25NEW889 supplierPartNumber: BrandNewPart123 radiusSize: 25 materialType: Aluminium color: Gray description: Updated item type: NewCategory oldType: Widget quantityOfItem: 80 unit: pcs price: 60 markUpPrice: 300 catCode: NEW889').scrollIntoView().should('be.visible');
     });
 
-    it('Successfully update product and verify in the logs page', () => {
+    it('Successfully Update Product Quantity and Verify in the Logs Page', () => {
         cy.contains('INVENTORY').click();
         cy.contains('C5WGT15').click();
 
@@ -178,7 +386,7 @@ describe('Logging the action of adding a new user to the database', () => {
     });
 
 
-    it('Successfully delete product and verify in the logs page', () => {
+    it('Successfully Delete Product C5WGT15 and Verify in the Logs Page', () => {
         cy.contains('INVENTORY').click();
         cy.contains('C5WGT15').click();
 
@@ -199,8 +407,28 @@ describe('Logging the action of adding a new user to the database', () => {
         cy.contains('Product Deleted part_number: C5WGT15 supplier_part_number: GTY-66498 radius_size: 5 material_type: Copper color: Copper description: This is a test product description type: Widget quantity_of_item: 15.00 unit: ft price: $10.20 mark_up_price: $13.26').should('be.visible');
     });
     
+    it('Successfully Delete Product A25NEW889 and Verify in the Logs Page', () => {
+        cy.contains('INVENTORY').click();
+        cy.contains('A25NEW889').click();
 
-    it('Successfully updated user informationa and verify in the logs page', () => {
+        cy.get('.delete-button').click();
+        cy.get('.delete-confirm').click();
+
+        
+        cy.contains('Product deleted successfully').should('be.visible');
+
+        cy.get('.Toastify__toast-container').within(() => {
+            cy.get('.Toastify__close-button').click();
+        });
+
+        cy.contains('LOGS').click();
+
+        cy.contains('Delete Product').scrollIntoView().should('be.visible');
+        cy.contains('Inventory').should('be.visible');
+        cy.contains('Product Deleted part_number: A25NEW889 supplier_part_number: BrandNewPart123 radius_size: 25 material_type: Aluminium color: Gray description: Updated item type: NewCategory quantity_of_item: 80.00 unit: pcs price: $60.00 mark_up_price: $300.00').should('be.visible');
+    });
+
+    it('Successfully Updated User Informationa and Verify in the Logs Page', () => {
         cy.get('.username').click();
         cy.get('.edit-profile').contains('Edit Profile').click();
 
