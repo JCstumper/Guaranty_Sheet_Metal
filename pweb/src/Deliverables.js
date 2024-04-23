57b77f4f2f19392373c95f31bcde8e89460dd397
Waleed Kambal <w01.kambal@gmail.com>
2024-04-23 02:00:46 -0500

    Updated pweb for S3

--- pweb/src/Deliverables.js
+++ pweb/src/Deliverables.js
@@ -54,22 +54,22 @@ function Deliverables() {
       </div>
       <div className="deliverable">
         <h3>Sprint 3 Deliverable</h3>
-        <a href={`${process.env.PUBLIC_URL}/Metals-Sprint3_SprintPlanningDocument.doc`} target="_blank" rel="noopener noreferrer" className="github-button">
+        <a href={`${process.env.PUBLIC_URL}/Sprint3PD.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
           Sprint 3 Planning Document
         </a>
-        <a href={`${process.env.PUBLIC_URL}/Metals-Sprint3_SprintGoalBacklog.doc`} target="_blank" rel="noopener noreferrer" className="github-button">
+        <a href={`${process.env.PUBLIC_URL}/Sprint3B.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
           Sprint 3 Backlog
         </a>
-        <a href={`${process.env.PUBLIC_URL}/DailyJournalDocument.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
+        <a href={`${process.env.PUBLIC_URL}/Sprint3DJ.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
           Updated Daily Journal
         </a>
-        <a href={`${process.env.PUBLIC_URL}/Metals-Sprint1Slides.pptx`} target="_blank" rel="noopener noreferrer" className="github-button">
+        <a href={`${process.env.PUBLIC_URL}/Sprint3Slide.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
           Sprint 3 PowerPoint
         </a>
-        <a href={`${process.env.PUBLIC_URL}/Metals-Sprint3_ReviewandRetro.doc`} target="_blank" rel="noopener noreferrer" className="github-button">
+        <a href={`${process.env.PUBLIC_URL}/Sprint3RaR.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
           Sprint 3 Retrospective
         </a>
-        <a href={`${process.env.PUBLIC_URL}/Metals-Sprint3_ProjectBacklog.xlsx`} target="_blank" rel="noopener noreferrer" className="github-button">
+        <a href={`${process.env.PUBLIC_URL}/Sprint3PB.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
           Project Backlog
         </a>
         <a href="https://github.com/JCstumper/Guaranty_Sheet_Metal/tree/sprint1snapshot" className="github-button">
@@ -77,6 +77,9 @@ function Deliverables() {
         </a>
         <a href={`${process.env.PUBLIC_URL}/Metals-Sprint3SponsorMeetings.pdf`} target="_blank" rel="noopener noreferrer" className="github-button">
           Sprint 3 Sponsor Feedback
+        </a>
+        <a href= "https://youtu.be/4YM1GAGqlUA" target="_blank" rel="noopener noreferrer" className="github-button">
+          Sprint 3 Video
         </a>
       </div>
     </div>
